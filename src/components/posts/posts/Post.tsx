00637326743d350599b35ad17c21b0e401d7726e
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-03-26 23:34:44 +0700

    посты

---
+++ src/components/posts/posts/Post.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import styles from './Post.module.scss';
+export type PostPropsType = {
+   avatar: string;
+   likes: number;
+};
+
+export const Post = (props: PostPropsType) => {
+   return (
+      <div>
+         <img alt="avatar" className={styles.postAvatar} src={props.avatar}></img>
+         <p className={styles.postText}>Hello, how are you?</p>
+         <p>likes: {props.likes}</p>
+      </div>
+   );
+};
