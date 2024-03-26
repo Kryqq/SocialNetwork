24ab7b1039bb364870e6d75b48ca82fcce1cc430
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-03-26 23:34:16 +0700

    добавление роутер дома

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
-import React from 'react';
 import './App.css';
 import { Header } from './layout/header/Header';
 import { Profile } from './layout/profile/Profile';
 import { Navbar } from './layout/navbar/Navbar';
+import { Dialogs } from './layout/dialogs/Dialogs';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { News } from './layout/news/News';
+import { Music } from './layout/music/Music';
+import { Settings } from './layout/settings/Settings';
+import React from 'react';
 
 function App() {
    return (
-      <div className="App">
-         <Header />
-         <Profile />
-         <Navbar />
-      </div>
+      <BrowserRouter>
+         <div className="App">
+            <Header />
+            <Navbar />
+            <div className="app-content">
+               <Route path="/dialogs" component={Dialogs} />
+               <Route path="/profile" component={Profile} />
+               <Route path="/news" component={News} />
+               <Route path="/music" component={Music} />
+               <Route path="/settings" component={Settings} />
+            </div>
+         </div>
+      </BrowserRouter>
    );
 }
 
