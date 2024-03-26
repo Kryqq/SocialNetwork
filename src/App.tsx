984fee31e56158962f101e3572d827951d61d148
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-03-26 22:14:19 +0700

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
+import { Header } from './layout/header/Header';
+import { Profile } from './layout/profile/Profile';
+import { Navbar } from './layout/navbar/Navbar';
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+   return (
+      <div className="App">
+         <Header />
+         <Profile />
+         <Navbar />
+      </div>
+   );
 }
 
 export default App;
