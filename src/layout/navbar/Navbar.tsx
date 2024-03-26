7cf036f21b1c71906e253cde550c5604ce253444
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-03-26 23:35:01 +0700

    добавление ссылок с использованием роутер дома

--- src/layout/navbar/Navbar.tsx
+++ src/layout/navbar/Navbar.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import styles from './Navbar.module.scss';
+import { Link } from 'react-router-dom';
 export const Navbar = () => {
    return (
       <div className={styles.navbar}>
          <ul>
-            <li>Profle</li>
-            <li>Messages</li>
-            <li>News</li>
-            <li>Music</li>
-            <li>Settings</li>
+            <li>
+               <Link to="/profile">Profle</Link>
+            </li>
+            <li>
+               <Link to="/dialogs">Messages</Link>
+            </li>
+            <li>
+               <Link to="/news">News</Link>
+            </li>
+            <li>
+               <Link to="/music">Music</Link>
+            </li>
+            <li>
+               <Link to="/settings">Settings</Link>
+            </li>
          </ul>
       </div>
    );
