a5be603782d20d338235db28e09fe56ea6718f19
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-03-27 22:00:59 +0700

    reafactoring

--- src/layout/profile/Profile.tsx
+++ src/layout/profile/Profile.tsx
@@ -1,23 +1,11 @@
 import React from 'react';
 import styles from './Profile.module.scss';
-import { Posts } from '../../components/posts/Posts';
+import { Posts } from './posts/Posts';
+import ProfileInfo from './profileInfo/ProfileInfo';
 export const Profile = () => {
    return (
       <div className={styles.profile}>
-         <div>
-            <img
-               className={styles.profileCover}
-               alt="profileCover"
-               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwM1pIUa4tHBRHNNfSlrch8EKLu68CNSaQ3A&usqp=CAU"
-            ></img>
-         </div>
-         <div className={styles.profileInfo}>
-            <img
-               className={styles.profileAvatar}
-               alt="avatar"
-               src="https://yt3.googleusercontent.com/-CFTJHU7fEWb7BYEb6Jh9gm1EpetvVGQqtof0Rbh-VQRIznYYKJxCaqv_9HeBcmJmIsp2vOO9JU=s900-c-k-c0x00ffffff-no-rj"
-            ></img>
-         </div>
+         <ProfileInfo />
          <Posts
             avatar={
                'https://yt3.googleusercontent.com/-CFTJHU7fEWb7BYEb6Jh9gm1EpetvVGQqtof0Rbh-VQRIznYYKJxCaqv_9HeBcmJmIsp2vOO9JU=s900-c-k-c0x00ffffff-no-rj'
---
+++ src/layout/profile/posts/Posts.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Post } from './post/Post';
+
+export type PostsPropsType = {
+   avatar: string;
+};
+
+export const Posts = (props: PostsPropsType) => {
+   return (
+      <div>
+         <div>
+            <textarea></textarea>
+            <button></button>
+         </div>
+         <Post avatar={props.avatar} likes={16} />
+         <Post avatar={props.avatar} likes={11} />
+         <Post avatar={props.avatar} likes={14} />
+         <Post avatar={props.avatar} likes={5} />
+         <Post avatar={props.avatar} likes={8} />
+      </div>
+   );
+};
