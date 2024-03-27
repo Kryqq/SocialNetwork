891becff5bd3085e6ffbeee7e2405d9495c885ed
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-03-27 22:00:39 +0700

    добавление "активной ссылки"

--- src/layout/navbar/Navbar.tsx
+++ src/layout/navbar/Navbar.tsx
@@ -1,24 +1,34 @@
 import React from 'react';
 import styles from './Navbar.module.scss';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 export const Navbar = () => {
    return (
       <div className={styles.navbar}>
          <ul>
             <li>
-               <Link to="/profile">Profle</Link>
+               <NavLink activeClassName={styles.active} to="/profile">
+                  Профиль
+               </NavLink>
             </li>
             <li>
-               <Link to="/dialogs">Messages</Link>
+               <NavLink activeClassName={styles.active} to="/dialogs">
+                  Сообщения
+               </NavLink>
             </li>
             <li>
-               <Link to="/news">News</Link>
+               <NavLink activeClassName={styles.active} to="/news">
+                  Новости
+               </NavLink>
             </li>
             <li>
-               <Link to="/music">Music</Link>
+               <NavLink activeClassName={styles.active} to="/music">
+                  Музыка
+               </NavLink>
             </li>
             <li>
-               <Link to="/settings">Settings</Link>
+               <NavLink activeClassName={styles.active} to="/settings">
+                  Настройки
+               </NavLink>
             </li>
          </ul>
       </div>
