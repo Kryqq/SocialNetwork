68cb441451d56caa07214b0798f564c01647758f
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-03-30 00:10:57 +0700

    addPostHanlderUI

--- src/layout/profile/posts/Posts.tsx
+++ src/layout/profile/posts/Posts.tsx
@@ -4,14 +4,23 @@ import { PostsDataType } from '../../../redux/state';
 
 type PostsPropsType = {
    postsData: PostsDataType[];
+   addPost: (newPostText: string) => void;
 };
 
+const ref: React.RefObject<HTMLTextAreaElement> = React.createRef();
+
 export const Posts = (props: PostsPropsType) => {
+	
+   const addPostHandler = ( ) => {
+      if (ref.current) {
+		props.addPost(ref.current.value);
+      }
+   };
    return (
       <div>
          <div>
-            <textarea></textarea>
-            <button></button>
+            <textarea ref={ref}></textarea>
+            <button onClick={addPostHandler}></button>
          </div>
          {props.postsData.map((post) => (
             <Post id={post.id} postAvatar={post.avatar} postTitle={post.postTitle} likes={post.likes} key={post.id} />
