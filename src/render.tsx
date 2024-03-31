a4d6dce3a407a6434c35083e404a812c79f52bca
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-03-31 22:16:38 +0700

    updatenewPostText()

--- src/render.tsx
+++ src/render.tsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-import { StateType, addPost } from './redux/state';
+import { StateType, addPost, updateNewPostText } from './redux/state';
 export const rerenderEntireTree = (state: StateType) => {
-   ReactDOM.render(<App state={state} addPost={addPost} />, document.getElementById('root'));
+   ReactDOM.render(<App state={state} addPost={addPost} updateNewPostText={updateNewPostText} />, document.getElementById('root'));
 };
 
 
