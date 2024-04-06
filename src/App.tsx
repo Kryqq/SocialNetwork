f138c93147f08c8d7b36fe99b2ae2d0e5a077a9b
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-04-06 21:12:36 +0700

    ✅ изменение props

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,20 @@ import './App.css';
 import { Header } from './layout/header/Header';
 import { Profile } from './layout/profile/Profile';
 import { Navbar } from './layout/navbar/Navbar';
-import { Dialogs } from './layout/dialogs/Dialogs';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { DialogsContainer } from './layout/dialogs/DialogsContainer';
+
 import { News } from './layout/news/News';
 import { Music } from './layout/music/Music';
 import { Settings } from './layout/settings/Settings';
 import { ActionType, StateType } from './redux/store';
+import { StoreType } from './redux/reduxStore';
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 type AppPropsType = {
    state: StateType;
    dispatch: (action: ActionType) => void;
+   store: StoreType;
 };
 
 function App(props: AppPropsType) {
@@ -22,11 +25,8 @@ function App(props: AppPropsType) {
             <Header />
             <Navbar />
             <div className="app-content">
-               <Route path="/dialogs" render={() => <Dialogs dialogsPage={props.state.dialogsPage} dispatch={props.dispatch} />} />
-               <Route
-                  path="/profile"
-                  render={() => <Profile profilePage={props.state.profilePage} dispatch={props.dispatch} />}
-               />
+               <Route path="/dialogs" render={() => <DialogsContainer store={props.store} />} />
+               <Route path="/profile" render={() => <Profile store={props.store} />} />
                <Route path="/news" render={() => <News />} />
                <Route path="/music" render={() => <Music />} />
                <Route path="/settings" render={() => <Settings />} />
