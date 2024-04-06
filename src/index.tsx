d3e88c25c061028d94cbcd328049e857890c3690
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-04-06 21:12:51 +0700

    ✅ пробрасывание store

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,15 @@ import { store } from './redux/reduxStore';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { StateType } from './redux/store';
+
 const rerenderEntireTree = (state: StateType) => {
-   ReactDOM.render(<App state={state} dispatch={store.dispatch.bind(store)} />, document.getElementById('root'));
+   ReactDOM.render(
+      <App store={store} state={state} dispatch={store.dispatch.bind(store)} />,
+      document.getElementById('root')
+   );
 };
 
-rerenderEntireTree(store.getState()); //1 для первого рендера
+rerenderEntireTree(store.getState());
 
 store.subscribe(() => {
    rerenderEntireTree(store.getState());
