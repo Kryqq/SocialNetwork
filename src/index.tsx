d9a4f524c7c273a713d1bcf4b87af166bb76882a
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-04-07 19:05:38 +0700

    ✅refactor: удаление subscibe

--- src/index.tsx
+++ src/index.tsx
@@ -2,23 +2,15 @@ import './index.css';
 import { store } from './redux/reduxStore';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { StateType } from './redux/store';
+
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
-const rerenderEntireTree = (state: StateType) => {
-   ReactDOM.render(
-      <BrowserRouter>
-         <Provider store={store}>
-            <App  />
-         </Provider>
-      </BrowserRouter>,
-      document.getElementById('root')
-   );
-};
-
-rerenderEntireTree(store.getState());
-
-store.subscribe(() => {
-   rerenderEntireTree(store.getState());
-});
+ReactDOM.render(
+   <BrowserRouter>
+      <Provider store={store}>
+         <App />
+      </Provider>
+   </BrowserRouter>,
+   document.getElementById('root')
+);
