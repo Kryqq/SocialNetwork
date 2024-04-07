634b1b6e60b2cfc8e7062051116e19df8a166b2b
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-04-07 21:41:08 +0700

    ✅ add usersPage

--- src/layout/users/Users.tsx
+++ src/layout/users/Users.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { UserType } from '../../redux/usersReducer';
 
-export const Users = () => {
-  return (
-    <div>
-	 users here
-    </div>
-  )
-}
+type UsersPropsType = {
+   users: Array<UserType>;
+   followUser: (userId: number) => void;
+   unfollowUser: (userId: number) => void;
+   setUsers: (users: Array<UserType>) => void;
+};
 
-
+export const Users = (props: UsersPropsType) => {
+   return (
+      <div>
+         {props.users.map((u: UserType) => (
+            <div>
+               <div key={u.id}>{u.fullName}</div>
+               {u.followed ? (
+                  <button onClick={() => props.unfollowUser(u.id)}>Unfollow</button>
+               ) : (
+                  <button onClick={() => props.followUser(u.id)}>Follow</button>
+               )}
+            </div>
+         ))}
+      </div>
+   );
+};
