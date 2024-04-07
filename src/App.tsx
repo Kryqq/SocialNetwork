b20e7079bb2854fbbc94c26a817377a9eb5f118c
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-04-07 16:45:04 +0700

    ✅ подключение redux-toolkita, вынос browser router в index.ts, удаление propstype

--- src/App.tsx
+++ src/App.tsx
@@ -7,32 +7,25 @@ import { DialogsContainer } from './layout/dialogs/DialogsContainer';
 import { News } from './layout/news/News';
 import { Music } from './layout/music/Music';
 import { Settings } from './layout/settings/Settings';
-import { ActionType, StateType } from './redux/store';
-import { StoreType } from './redux/reduxStore';
+
+import { Route } from 'react-router-dom';
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
 
-type AppPropsType = {
-   state: StateType;
-   dispatch: (action: ActionType) => void;
-   store: StoreType;
-};
 
-function App(props: AppPropsType) {
+
+function App() {
    return (
-      <BrowserRouter>
-         <div className="App">
-            <Header />
-            <Navbar />
-            <div className="app-content">
-               <Route path="/dialogs" render={() => <DialogsContainer store={props.store} />} />
-               <Route path="/profile" render={() => <Profile store={props.store} />} />
-               <Route path="/news" render={() => <News />} />
-               <Route path="/music" render={() => <Music />} />
-               <Route path="/settings" render={() => <Settings />} />
-            </div>
+      <div className="App">
+         <Header />
+         <Navbar />
+         <div className="app-content">
+            <Route path="/dialogs" render={() => <DialogsContainer />} />
+            <Route path="/profile" render={() => <Profile  />} />
+            <Route path="/news" render={() => <News />} />
+            <Route path="/music" render={() => <Music />} />
+            <Route path="/settings" render={() => <Settings />} />
          </div>
-      </BrowserRouter>
+      </div>
    );
 }
 
