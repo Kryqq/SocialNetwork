a2ca9a03b0d9eaba4a0e2ef23cad8201d15f0e48
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-04-09 21:25:07 +0700

    ✅refactor: типизация

--- src/redux/usersReducer.ts
+++ src/redux/usersReducer.ts
@@ -1,3 +1,4 @@
+import { Settings } from 'http2';
 import { ActionType } from './store';
 
 const FOLLOW = 'FOLLOW';
@@ -5,56 +6,22 @@ const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET-USERS';
 
 export type UserType = {
+   name: string;
    id: number;
-   avatar: string;
-   followed: boolean;
-   fullName: string;
-   status: string;
-   location: {
-      city: string;
-      country: string;
+   uniqueUrlName: string;
+   photos: {
+      small: string;
+      large: string;
    };
+   status: null;
+   followed: boolean;
 };
 type usersStateType = {
    users: Array<UserType>;
 };
 
 const initialState: usersStateType = {
-   users: [
-      {
-         id: 1,
-         avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3C2T99qXhPBAwmdgSk6Uc5CcG85ea99bLBP1W67Rorw&s',
-         followed: false,
-         fullName: 'Вася',
-         status: "Hi! i'am here!!!",
-         location: { city: 'Москва', country: 'Россия' },
-      },
-      {
-         id: 2,
-         avatar: 'https://i.imgur.com/B4wQENm.png',
-         followed: true,
-         fullName: 'Петя',
-         status: "Hi! i'am here tooo!!!",
-         location: { city: 'Питер', country: 'Россия' },
-      },
-      {
-         id: 3,
-         avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkm0nHxzF3ZOMCjlMBE7Tzje2JPKvX493QMN_duxL8xg&s',
-         followed: false,
-         fullName: 'Женя',
-         status: "Hi! i'am here!!!",
-         location: { city: 'Минск', country: 'Беларусь' },
-      },
-      {
-         id: 4,
-         avatar:
-            'https://preview.redd.it/i-rendered-a-3d-version-of-the-default-green-discord-v0-0jri55mmvb6a1.png?width=640&crop=smart&auto=webp&s=3dfde767af030266a91ff28f6a13060017a361f7',
-         followed: true,
-         fullName: 'Лена',
-         status: "Hi! i'am here!!!",
-         location: { city: 'Тула', country: 'Россия' },
-      },
-   ],
+   users: [],
 };
 
 export const usersReducer = (state = initialState, action: ActionType) => {
