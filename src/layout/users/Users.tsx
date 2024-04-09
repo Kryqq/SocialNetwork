2d0608f2476326214094ef42efcb00a45a55ef6a
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-04-09 21:23:27 +0700

    ✅ refactor:  классовая компонента

--- src/layout/users/Users.tsx
+++ src/layout/users/Users.tsx
@@ -1,51 +1,45 @@
-import React from 'react';
 import { UserType } from '../../redux/usersReducer';
 import styles from './Users.module.scss';
-
+import axios from 'axios';
+import userPhoto from '../../assets/userPhoto.webp';
+import React from 'react';
 type UsersPropsType = {
    users: Array<UserType>;
    followUser: (userId: number) => void;
    unfollowUser: (userId: number) => void;
    setUsers: (users: Array<UserType>) => void;
 };
+export class Users extends React.Component<UsersPropsType> {
+   constructor(props: UsersPropsType) {
+      super(props);
 
-export const Users = (props: UsersPropsType) => {
-   return (
-      <div className={styles.usersWrapper}>
-         {props.users.map((u: UserType) => (
-            <div key={u.id} className={styles.userTable}>
-               <div className={styles.userCard}>
-                  <img src={u.avatar} alt={'avatar'}></img>
-                  <span className={styles.userName}>{u.fullName} </span>
-                  {u.followed ? (
-                     <button onClick={() => props.unfollowUser(u.id)}>Unfollow</button>
-                  ) : (
-                     <button onClick={() => props.followUser(u.id)}>Follow</button>
-                  )}
-               </div>
-               <div className={styles.userInfo}>
-                  <div>
-                     <span>Status: {u.status} </span>
-                     <span>{u.location.city}</span>
-                     <span>{u.location.country}</span>
+      axios.get('https://social-network.samuraijs.com/api/1.0/users').then((response) => {
+         this.props.setUsers(response.data.items);
+      });
+   }
+   getUsers = () => {};
+   render() {
+      return (
+         <div className={styles.usersWrapper}>
+            {this.props.users.map((u: UserType) => (
+               <div key={u.id} className={styles.userTable}>
+                  <div className={styles.userCard}>
+                     <img src={u.photos.small !== null ? u.photos.small : userPhoto} alt={'avatar'}></img>
+                     <span className={styles.userName}>{u.name} </span>
+                     {u.followed ? (
+                        <button onClick={() => this.props.unfollowUser(u.id)}>Unfollow</button>
+                     ) : (
+                        <button onClick={() => this.props.followUser(u.id)}>Follow</button>
+                     )}
+                  </div>
+                  <div className={styles.userInfo}>
+                     <div>
+                        <span>Status: {u.status} </span>
+                     </div>
                   </div>
                </div>
-            </div>
-         ))}
-      </div>
-   );
-};
-{
-   /* <div key={u.id} className={styles.userTable}>
-<div className={styles.user}>
-   <img src={u.avatar} alt="avatar"></img>
-   <span>{u.fullName} </span>
-   {u.followed ? (
-	 <button onClick={() => props.unfollowUser(u.id)}>Unfollow</button>
-   ) : (
-	 <button onClick={() => props.followUser(u.id)}>Follow</button>
-   )}
-   <div>asd</div>
-</div>
-</div> */
+            ))}
+         </div>
+      );
+   }
 }
