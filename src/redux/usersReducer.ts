ded0ddafbda49893b8bc8c7b145843c5a7f27474
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-04-10 19:57:00 +0700

    ✅ Добавление функционала usersPage + типизация

--- src/redux/usersReducer.ts
+++ src/redux/usersReducer.ts
@@ -1,13 +1,15 @@
-import { Settings } from 'http2';
 import { ActionType } from './store';
 
-const FOLLOW = 'FOLLOW';
-const UNFOLLOW = 'UNFOLLOW';
-const SET_USERS = 'SET-USERS';
+const FOLLOW = 'FOLLOW' as const;
+const UNFOLLOW = 'UNFOLLOW' as const;
+const SET_USERS = 'SET-USERS' as const;
+const SET_CURRENT_PAGE = 'SET-CURRENT-PAGE' as const;
+const SET_TOTAL_USERS_COUNT = 'SET-TOTAL-USERS-COUNT' as const;
 
 export type UserType = {
    name: string;
    id: number;
+
    uniqueUrlName: string;
    photos: {
       small: string;
@@ -18,10 +20,16 @@ export type UserType = {
 };
 type usersStateType = {
    users: Array<UserType>;
+   totalUsersCount: number;
+   pageSize: number;
+   currentPage: number;
 };
 
 const initialState: usersStateType = {
    users: [],
+   pageSize: 5,
+   totalUsersCount: 0,
+   currentPage: 1,
 };
 
 export const usersReducer = (state = initialState, action: ActionType) => {
@@ -47,7 +55,11 @@ export const usersReducer = (state = initialState, action: ActionType) => {
             }),
          };
       case SET_USERS:
-         return { ...state, users: [...state.users, ...action.users] };
+         return { ...state, users: action.users };
+      case SET_CURRENT_PAGE:
+         return { ...state, currentPage: action.currentPage };
+      case SET_TOTAL_USERS_COUNT:
+         return { ...state, totalUsersCount: action.count };
 
       default:
          return state;
@@ -67,19 +79,39 @@ export interface ISET_USERS {
    type: 'SET-USERS';
    users: Array<UserType>;
 }
+export interface ISET_CURRENT_PAGE {
+   type: 'SET-CURRENT-PAGE';
+   currentPage: number;
+}
+export interface ISET_TOTAL_USERS_COUNT {
+   type: 'SET-TOTAL-USERS-COUNT';
+   count: number;
+}
 
 export type followUserActionCreatorType = (userId: number) => IFOLLOW_USER;
 export type unfollowUserActionCreatorType = (userId: number) => IUNFOLLOW_USER;
 export type setUsersActionCreatorType = (users: Array<UserType>) => ISET_USERS;
+export type setCurrentPageActionCreatorType = (currentPage: number) => ISET_CURRENT_PAGE;
+export type setTotalUsersCount = (totalUsersCount: number) => ISET_TOTAL_USERS_COUNT;
 
 export const setUserActionCreator: setUsersActionCreatorType = (users: Array<UserType>) => ({
    type: SET_USERS,
-   users: users,
+   users,
 });
 
 export const followUserActionCreator: followUserActionCreatorType = (userId: number) => ({ type: FOLLOW, userId });
 
 export const unfollowUserActionCreator: unfollowUserActionCreatorType = (userId: number) => ({
    type: UNFOLLOW,
-   userId: userId,
+   userId,
+});
+
+export const setCurrentPageActionCreator: setCurrentPageActionCreatorType = (currentPage: number) => ({
+   type: SET_CURRENT_PAGE,
+   currentPage,
+});
+
+export const setTotalUsersCountActionCreator = (totalUsersCount: number) => ({
+   type: SET_TOTAL_USERS_COUNT,
+   count: totalUsersCount,
 });
