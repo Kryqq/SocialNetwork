9c76a923f679f01ae2d78eb6faed6d2c596ede1c
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-04-13 21:12:22 +0700

    ✅Типизация Action

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,5 @@
 import { ISEND_PRIVATE_MESSAGE, IUPDATE_NEW_MESSAGE_TEXT, dialogsReducer } from './dialogsReducer';
-import { IADD_POST, IUPDATE_NEW_POST_TEXT, profileReducer } from './profileReducer';
+import { IADD_POST, IUPDATE_NEW_POST_TEXT, profileReducer, ISET_USER_PROFILE, ProfileType } from './profileReducer';
 import {
    IFOLLOW_USER,
    ISET_CURRENT_PAGE,
@@ -19,7 +19,8 @@ export type ActionType =
    | ISET_USERS
    | ISET_CURRENT_PAGE
    | ISET_TOTAL_USERS_COUNT
-   | ISET_IS_FETCHING;
+   | ISET_IS_FETCHING
+   | ISET_USER_PROFILE;
 
 export type PostsDataType = {
    id: number;
@@ -31,6 +32,7 @@ export type PostsDataType = {
 export type ProfilePageType = {
    postsData: PostsDataType[];
    newPostText: string;
+   profile: null | ProfileType;
 };
 
 export type DialogsDataType = {
@@ -64,72 +66,72 @@ export type StoreType = {
    dispatch: (action: ActionType) => void;
 };
 
-export const store: StoreType = {
-   _state: {
-      profilePage: {
-         postsData: [
-            {
-               id: 1,
-               postTitle: 'Hello, how are you?',
-               avatar:
-                  'https://yt3.googleusercontent.com/-CFTJHU7fEWb7BYEb6Jh9gm1EpetvVGQqtof0Rbh-VQRIznYYKJxCaqv_9HeBcmJmIsp2vOO9JU=s900-c-k-c0x00ffffff-no-rj',
-               likes: 16,
-            },
-            {
-               id: 2,
-               postTitle: 'now im using whats app ;)0?',
-               avatar:
-                  'https://yt3.googleusercontent.com/-CFTJHU7fEWb7BYEb6Jh9gm1EpetvVGQqtof0Rbh-VQRIznYYKJxCaqv_9HeBcmJmIsp2vOO9JU=s900-c-k-c0x00ffffff-no-rj',
-               likes: 11,
-            },
-            {
-               id: 3,
-               postTitle: 'yoyoy?',
-               avatar:
-                  'https://yt3.googleusercontent.com/-CFTJHU7fEWb7BYEb6Jh9gm1EpetvVGQqtof0Rbh-VQRIznYYKJxCaqv_9HeBcmJmIsp2vOO9JU=s900-c-k-c0x00ffffff-no-rj',
-               likes: 14,
-            },
+// export const store: StoreType = {
+//    _state: {
+//       profilePage: {
+//          postsData: [
+//             {
+//                id: 1,
+//                postTitle: 'Hello, how are you?',
+//                avatar:
+//                   'https://yt3.googleusercontent.com/-CFTJHU7fEWb7BYEb6Jh9gm1EpetvVGQqtof0Rbh-VQRIznYYKJxCaqv_9HeBcmJmIsp2vOO9JU=s900-c-k-c0x00ffffff-no-rj',
+//                likes: 16,
+//             },
+//             {
+//                id: 2,
+//                postTitle: 'now im using whats app ;)0?',
+//                avatar:
+//                   'https://yt3.googleusercontent.com/-CFTJHU7fEWb7BYEb6Jh9gm1EpetvVGQqtof0Rbh-VQRIznYYKJxCaqv_9HeBcmJmIsp2vOO9JU=s900-c-k-c0x00ffffff-no-rj',
+//                likes: 11,
+//             },
+//             {
+//                id: 3,
+//                postTitle: 'yoyoy?',
+//                avatar:
+//                   'https://yt3.googleusercontent.com/-CFTJHU7fEWb7BYEb6Jh9gm1EpetvVGQqtof0Rbh-VQRIznYYKJxCaqv_9HeBcmJmIsp2vOO9JU=s900-c-k-c0x00ffffff-no-rj',
+//                likes: 14,
+//             },
 
-            {
-               id: 4,
-               postTitle: 'good weather today!',
-               avatar:
-                  'https://yt3.googleusercontent.com/-CFTJHU7fEWb7BYEb6Jh9gm1EpetvVGQqtof0Rbh-VQRIznYYKJxCaqv_9HeBcmJmIsp2vOO9JU=s900-c-k-c0x00ffffff-no-rj',
-               likes: 5,
-            },
-         ],
-         newPostText: '',
-      },
-      dialogsPage: {
-         messagesData: [
-            { id: 1, message: 'Hi' },
-            { id: 2, message: 'Hello' },
-            { id: 3, message: 'How are you?' },
-            { id: 4, message: 'I am fine' },
-         ],
-         dialogsData: [
-            { id: 1, name: 'Вася' },
-            { id: 2, name: 'Петя' },
-            { id: 3, name: 'Женя' },
-            { id: 4, name: 'Лена' },
-            { id: 5, name: 'Игорь' },
-         ],
-         newMessageText: '',
-      },
-   },
-   getState() {
-      return this._state;
-   },
-   _callSubscriber() {},
-   subscribe(observer: (state: StateType) => void) {
-      this._callSubscriber = observer;
-   },
+//             {
+//                id: 4,
+//                postTitle: 'good weather today!',
+//                avatar:
+//                   'https://yt3.googleusercontent.com/-CFTJHU7fEWb7BYEb6Jh9gm1EpetvVGQqtof0Rbh-VQRIznYYKJxCaqv_9HeBcmJmIsp2vOO9JU=s900-c-k-c0x00ffffff-no-rj',
+//                likes: 5,
+//             },
+//          ],
+//          newPostText: '',
+//       },
+//       dialogsPage: {
+//          messagesData: [
+//             { id: 1, message: 'Hi' },
+//             { id: 2, message: 'Hello' },
+//             { id: 3, message: 'How are you?' },
+//             { id: 4, message: 'I am fine' },
+//          ],
+//          dialogsData: [
+//             { id: 1, name: 'Вася' },
+//             { id: 2, name: 'Петя' },
+//             { id: 3, name: 'Женя' },
+//             { id: 4, name: 'Лена' },
+//             { id: 5, name: 'Игорь' },
+//          ],
+//          newMessageText: '',
+//       },
+//    },
+//    getState() {
+//       return this._state;
+//    },
+//    _callSubscriber() {},
+//    subscribe(observer: (state: StateType) => void) {
+//       this._callSubscriber = observer;
+//    },
 
-   dispatch(action) {
-      this._state.profilePage = profileReducer(this._state.profilePage, action);
+//    dispatch(action) {
+//       this._state.profilePage = profileReducer(this._state.profilePage, action);
 
-      this._state.dialogsPage = dialogsReducer(this._state.dialogsPage, action);
+//       this._state.dialogsPage = dialogsReducer(this._state.dialogsPage, action);
 
-      this._callSubscriber(this._state);
-   },
-};
+//       this._callSubscriber(this._state);
+//    },
+// };
