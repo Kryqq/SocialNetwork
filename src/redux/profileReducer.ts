5de5addc4d2972437c10bbd5d4a55402916dcc80
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-04-13 21:12:05 +0700

    ✅Типизация профиля юзера

--- src/redux/profileReducer.ts
+++ src/redux/profileReducer.ts
@@ -3,6 +3,31 @@ import { ActionType, ProfilePageType } from './store';
 export const ADD_POST = 'ADD-POST';
 
 export const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+export const SET_USER_PROFILE = 'SET_USER_PROFILE';
+
+type ContactsType = {
+   facebook: string;
+   website: null;
+   vk: string;
+   twitter: string;
+   instagram: string;
+   youtube: null;
+   github: string;
+   mainLink: null;
+};
+
+type PhotosType = {
+   small: string;
+   large: string;
+};
+
+export type ProfileType = {
+   aboutMe: string;
+   contacts: ContactsType;
+   lookingForAJob: boolean;
+   lookingForAJobDescription: string;
+   photos: PhotosType;
+};
 
 const initialState: ProfilePageType = {
    postsData: [
@@ -37,6 +62,7 @@ const initialState: ProfilePageType = {
       },
    ],
    newPostText: '',
+   profile: null,
 };
 
 export const profileReducer = (state = initialState, action: ActionType) => {
@@ -58,7 +84,9 @@ export const profileReducer = (state = initialState, action: ActionType) => {
 
       case UPDATE_NEW_POST_TEXT:
          return { ...state, newPostText: action.newText };
-	    
+      case SET_USER_PROFILE:
+         return { ...state, profile: action.profile };
+
       default:
          return state;
    }
@@ -68,17 +96,27 @@ export interface IADD_POST {
    type: 'ADD-POST';
 }
 
-
 export interface IUPDATE_NEW_POST_TEXT {
    type: 'UPDATE-NEW-POST-TEXT';
    newText: string;
 }
+
+export interface ISET_USER_PROFILE {
+   type: 'SET_USER_PROFILE';
+   profile: ProfileType;
+}
+
 export type addPostActionCreatorType = () => IADD_POST;
 export type UpdateNewPostTextActionCreatorType = (text: string) => IUPDATE_NEW_POST_TEXT;
+export type setUserProfileActionCreatorType = (profile: ProfileType) => ISET_USER_PROFILE;
 
 export const addPostActionCreator: addPostActionCreatorType = () => ({ type: ADD_POST });
 
 export const updateNewPostTextActionCreator: UpdateNewPostTextActionCreatorType = (text: string) => ({
    type: UPDATE_NEW_POST_TEXT,
    newText: text,
+});
+export const setUserProfile = (profile: ProfileType) => ({
+   type: SET_USER_PROFILE,
+   profile,
 });
