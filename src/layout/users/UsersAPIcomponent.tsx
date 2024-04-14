5ea9dbe914698acec0517a44200bfac4aab104f5
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-04-14 20:52:20 +0700

    ✅Отключение кнопки при подписке/отписке

--- src/layout/users/UsersAPIcomponent.tsx
+++ src/layout/users/UsersAPIcomponent.tsx
@@ -3,44 +3,38 @@ import { UserType } from '../../redux/usersReducer';
 import axios from 'axios';
 import { Users } from './Users';
 import { Preloader } from '../components/common/preloader/Preloader';
+import { usersAPI } from '../../api/api';
 type UsersPropsType = {
    users: Array<UserType>;
    pageSize: number;
    currentPage: number;
    totalCount: number;
    isFetching: boolean;
+   followingInProgress: Array<number>;
    followUser: (userId: number) => void;
    unfollowUser: (userId: number) => void;
    setUsers: (users: Array<UserType>) => void;
    setCurrentPage: (pageNumber: number) => void;
    setTotalUsersCount: (totalCount: number) => void;
    toggleIsFetching: (isFetching: boolean) => void;
+   toggleFollowingProgress: (isFetching: boolean, userId: number) => void;
 };
 export class UsersAPIcomponent extends React.Component<UsersPropsType> {
    componentDidMount() {
       this.props.toggleIsFetching(true);
-      axios
-         .get(
-            `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`,
-            {
-               withCredentials: true,
-            }
-         )
-         .then((response) => {
-            this.props.toggleIsFetching(false);
-            this.props.setUsers(response.data.items);
-            this.props.setTotalUsersCount(response.data.totalCount);
-         });
+      usersAPI.getUsers(this.props.currentPage, this.props.pageSize).then((data) => {
+         this.props.toggleIsFetching(false);
+         this.props.setUsers(data.items);
+         this.props.setTotalUsersCount(data.totalCount);
+      });
    }
    onPageChanged = (pageNumber: number) => {
       this.props.toggleIsFetching(true);
       this.props.setCurrentPage(pageNumber);
-      axios
-         .get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
-         .then((response) => {
-            this.props.toggleIsFetching(false);
-            this.props.setUsers(response.data.items);
-         });
+      usersAPI.getUsers(pageNumber, this.props.pageSize).then((data) => {
+         this.props.toggleIsFetching(false);
+         this.props.setUsers(data.items);
+      });
    };
    render() {
       return (
@@ -50,10 +44,12 @@ export class UsersAPIcomponent extends React.Component<UsersPropsType> {
                users={this.props.users}
                pageSize={this.props.pageSize}
                currentPage={this.props.currentPage}
-               onPageChanged={this.onPageChanged}
                totalCount={this.props.totalCount}
+               followingInProgress={this.props.followingInProgress}
+               onPageChanged={this.onPageChanged}
                followUser={this.props.followUser}
                unfollowUser={this.props.unfollowUser}
+               toggleFollowingProgress={this.props.toggleFollowingProgress}
             />
          </>
       );
