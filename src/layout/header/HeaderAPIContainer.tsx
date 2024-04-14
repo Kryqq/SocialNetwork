9e681f020fbbc2507dfda579c39debde039a790b
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-04-14 17:14:00 +0700

    ✅ создание Header API сomponent

---
+++ src/layout/header/HeaderAPIContainer.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Header } from './Header';
+import { AuthReducerType } from '../../redux/authReducer';
+import axios from 'axios';
+
+type HeaderPropsType = {
+   isAuth: boolean;
+   login: string | null;
+   setUserAuthData: (data: AuthReducerType) => void;
+};
+
+export class HeaderAPIContainer extends React.Component<HeaderPropsType> {
+   componentDidMount() {
+      axios.get('https://social-network.samuraijs.com/api/1.0/auth/me', { withCredentials: true }).then((response) => {
+         debugger;
+         if (response.data.resultCode === 0) {
+            this.props.setUserAuthData(response.data.data);
+         }
+      });
+   }
+   render() {
+      return <Header {...this.props} />;
+   }
+}
