d169ebc95a1e467c66a11137085b354ca3ff3811
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-04-14 17:15:48 +0700

    ✅ Добавление authReducera в Redux-Store

--- src/redux/reduxStore.ts
+++ src/redux/reduxStore.ts
@@ -3,14 +3,16 @@ import { combineReducers, createStore } from 'redux';
 import { profileReducer } from './profileReducer';
 import { dialogsReducer } from './dialogsReducer';
 import { usersReducer } from './usersReducer';
+import { authReducer } from './authReducer';
 
 const reducers = combineReducers({
    profilePage: profileReducer,
    dialogsPage: dialogsReducer,
-   usersPage: usersReducer
+   usersPage: usersReducer,
+   auth: authReducer,
 });
 
 export const store = createStore(reducers);
 export type StateType = ReturnType<typeof reducers>;
-export type StoreType = typeof store
-export type DispatchType = StoreType['dispatch']
+export type StoreType = typeof store;
+export type DispatchType = StoreType['dispatch'];
