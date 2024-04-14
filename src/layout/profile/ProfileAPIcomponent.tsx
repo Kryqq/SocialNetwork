da09db51b0f3e23e384535b9da9ee608d6ba3f9f
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-04-14 14:19:36 +0700

    ✅ добавление userId из withRouter

--- src/layout/profile/ProfileAPIcomponent.tsx
+++ src/layout/profile/ProfileAPIcomponent.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
-import { Profile } from './Profile';
 import axios from 'axios';
+import { Profile } from './Profile';
 import { ProfileType } from '../../redux/profileReducer';
-
+import { RouteComponentProps } from 'react-router-dom';
+interface RouteParams {
+   userId: string;
+}
 type ProfilePropsType = {
    setUserProfile: (profile: ProfileType) => void;
    profile: ProfileType | null;
 };
-export class ProfileApiComponent extends React.Component<ProfilePropsType> {
+export class ProfileApiComponent extends React.Component<ProfilePropsType & RouteComponentProps<RouteParams>> {
    componentDidMount() {
-    
+      let userId = this.props.match.params.userId;
+      if (!userId) {
+         userId = '2';
+      }
 
-      axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`).then((response) => {
+      axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`).then((response) => {
          this.props.setUserProfile(response.data);
       });
    }
