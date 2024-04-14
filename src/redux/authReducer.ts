51516827bfae361fbee1301931429280fad16ca0
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-04-14 17:15:05 +0700

    ✅ создание authReducer

---
+++ src/redux/authReducer.ts
@@ -0,0 +1,37 @@
+import { ActionType } from './store';
+
+const SET_USER_DATA = 'SET_USER_DATA' as const;
+
+export type AuthReducerType = {
+   userId: null | number;
+   email: null | string;
+   login: null | string;
+   isAuth: boolean;
+};
+
+const initialState: AuthReducerType = {
+   userId: null,
+   email: null,
+   login: null,
+   isAuth: false,
+};
+
+export const authReducer = (state = initialState, action: ActionType) => {
+   switch (action.type) {
+      case SET_USER_DATA:
+         return { ...state, ...action.data, isAuth: true };
+      default:
+         return state;
+   }
+};
+
+export interface ISET_USER_DATA {
+   type: typeof SET_USER_DATA;
+   data: AuthReducerType;
+}
+export type setUserDataActionCreatorType = (data: AuthReducerType) => ISET_USER_DATA;
+
+export const setUserAuthData: setUserDataActionCreatorType = (data: AuthReducerType) => ({
+   type: SET_USER_DATA,
+   data,
+});
