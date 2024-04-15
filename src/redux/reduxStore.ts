880863adae5360d725b7c292766330dede317c00
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-04-15 21:39:41 +0700

    ✅refactor (thunks)

--- src/redux/reduxStore.ts
+++ src/redux/reduxStore.ts
@@ -1,5 +1,5 @@
-import { combineReducers, createStore } from 'redux';
-
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import thunkMiddleware from 'redux-thunk';
 import { profileReducer } from './profileReducer';
 import { dialogsReducer } from './dialogsReducer';
 import { usersReducer } from './usersReducer';
@@ -12,7 +12,7 @@ const reducers = combineReducers({
    auth: authReducer,
 });
 
-export const store = createStore(reducers);
+export const store = createStore(reducers, applyMiddleware(thunkMiddleware));
 export type StateType = ReturnType<typeof reducers>;
 export type StoreType = typeof store;
 export type DispatchType = StoreType['dispatch'];
