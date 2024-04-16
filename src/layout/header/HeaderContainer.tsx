a3a6e40edb937b53da1ded5eb0fdffbe47831f71
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-04-16 22:43:00 +0700

    ✅refactor

--- src/layout/header/HeaderContainer.tsx
+++ src/layout/header/HeaderContainer.tsx
@@ -1,11 +1,11 @@
 import { connect } from 'react-redux';
 import { HeaderAPIContainer } from './HeaderAPIContainer';
-import {  setUserAuthData } from '../../redux/authReducer';
 import { StateType } from '../../redux/reduxStore';
+import { setUsersAuthThunkCreator } from '../../redux/authReducer';
 
 const mapStateToProps = (state: StateType) => ({
    isAuth: state.auth.isAuth,
    login: state.auth.login,
 });
 
-export const HeaderContainer = connect(mapStateToProps, { setUserAuthData })(HeaderAPIContainer);
+export const HeaderContainer = connect(mapStateToProps, { setUsersAuthThunkCreator })(HeaderAPIContainer);
