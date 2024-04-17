6d9b5761d1fa742f8cc73d70ccd0d4d26f5881f3
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-04-17 19:17:42 +0700

    ✅refactor

--- src/layout/profile/ProfileContainer.tsx
+++ src/layout/profile/ProfileContainer.tsx
@@ -1,13 +1,18 @@
-import { setUserProfile, setUserProfileThunkCreator } from '../../redux/profileReducer';
+import { setUserProfileThunkCreator } from '../../redux/profileReducer';
 import { ProfileApiComponent } from './ProfileAPIcomponent';
 import { StateType } from '../../redux/reduxStore';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import { WithAuthRedirect } from '../../HOC/WithAuthRedirect';
+import { compose } from 'redux';
+import { ComponentType } from 'react';
 
 const mapStateToProps = (state: StateType) => ({
    profile: state.profilePage.profile,
 });
 
-const withUrlDataContainerComponent = withRouter(ProfileApiComponent);
-
-export const ProfileContainer = connect(mapStateToProps, { setUserProfileThunkCreator })(withUrlDataContainerComponent);
+export const ProfileContainer = compose<ComponentType>(
+   connect(mapStateToProps, { setUserProfileThunkCreator }),
+   WithAuthRedirect,
+   withRouter
+)(ProfileApiComponent);
