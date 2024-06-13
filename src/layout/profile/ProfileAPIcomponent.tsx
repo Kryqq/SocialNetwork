f34d7ac53fc35fba3b84e9275524db5c65cb8ebe
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-06-13 21:34:27 +0700

    refactor

--- src/layout/profile/ProfileAPIcomponent.tsx
+++ src/layout/profile/ProfileAPIcomponent.tsx
@@ -8,19 +8,22 @@ interface RouteParams {
 }
 type ProfilePropsType = {
    setUserProfileThunkCreator: (userId: string) => void;
+   getStatusThunkCreator: (userId: string) => void;
+   updateStatusThunkCreator: (status: string) => void;
    profile: ProfileType | null;
+   status: string;
 };
 export class ProfileApiComponent extends React.Component<ProfilePropsType & RouteComponentProps<RouteParams>> {
    componentDidMount() {
       let userId = this.props.match.params.userId;
       if (!userId) {
-         userId = '2';
+         userId = '31076';
       }
 
       this.props.setUserProfileThunkCreator(userId);
-	 this.props.getUserStatus(userId)
+      this.props.getStatusThunkCreator(userId);
    }
    render() {
-      return <Profile {...this.props} />;
+      return <Profile {...this.props} status={this.props.status} updateStatus={this.props.updateStatusThunkCreator} />;
    }
 }
